d3203916710ee42c46173e8fc21f5101d2d086c5
GeorgOfenbeck <ofenbeck@inf.ethz.ch>
2015-07-15 20:10:13 +0200

    first draft of the DSL

--- src/main/spirals/dsls/SPL_DSL.scala
+++ src/main/spirals/dsls/SPL_DSL.scala
@@ -4,5 +4,12 @@ import scala.lms.internal.InternalFunctionsExp
 
 trait SPL_DSL extends BaseExp with InternalFunctionsExp {
 
+  implicit def SPLtoExp(i: SPL): Exp[SPL] = unit(i)
+
+  def tensor(x: Exp[SPL], y: Exp[SPL]): Exp[SPL] = Tensor(x, y)
+  def compose(x: Exp[SPL], y: Exp[SPL]): Exp[SPL] = Compose(x, y)
+
+  case class Tensor(x: Exp[SPL], y: Exp[SPL]) extends Def[SPL]
+  case class Compose(x: Exp[SPL], y: Exp[SPL]) extends Def[SPL]
 }
 
--- src/main/tutorial/IntroDSLStaging.scala
+++ src/main/tutorial/IntroDSLStaging.scala
@@ -5,6 +5,8 @@ import ch.ethz.spirals.rewrites._
 
 object IntroDSLStaging extends App {
 
-  val wht4 = (F_2 tensor I(2) ) compose (I(2) tensor F_2) //what we would like to have
-
+//  val wht4 = (F_2 tensor I(2) ) compose (I(2) tensor F_2) //what we would like to have
+  class MyDSLProgram extends SPL_DSL{
+    val wht4 = tensor(F_2(), I(2))
+  }
 }
