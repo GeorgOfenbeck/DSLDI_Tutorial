1c97c2803b8f6de9dde35dc2dad7eb969c3b605f
GeorgOfenbeck <ofenbeck@inf.ethz.ch>
2015-07-15 20:16:30 +0200

    setup for debugging session

--- src/main/spirals/dsls/SPL_DSL.scala
+++ src/main/spirals/dsls/SPL_DSL.scala
@@ -6,7 +6,10 @@ trait SPL_DSL extends BaseExp with InternalFunctionsExp {
 
   implicit def SPLtoExp(i: SPL): Exp[SPL] = unit(i)
 
-  def tensor(x: Exp[SPL], y: Exp[SPL]): Exp[SPL] = Tensor(x, y)
+  def tensor(x: Exp[SPL], y: Exp[SPL]): Exp[SPL] = {
+    val tmp = Tensor(x, y)
+    tmp
+  }
   def compose(x: Exp[SPL], y: Exp[SPL]): Exp[SPL] = Compose(x, y)
 
   case class Tensor(x: Exp[SPL], y: Exp[SPL]) extends Def[SPL]
--- src/main/tutorial/IntroDSLStaging.scala
+++ src/main/tutorial/IntroDSLStaging.scala
@@ -7,6 +7,18 @@ object IntroDSLStaging extends App {
 
 //  val wht4 = (F_2 tensor I(2) ) compose (I(2) tensor F_2) //what we would like to have
   class MyDSLProgram extends SPL_DSL{
-    val wht4 = tensor(F_2(), I(2))
+
+    def myprog() = {
+      val leftside = tensor(F_2(), I(2))
+      val rightside = tensor(I(2), F_2())
+      val wht4 = compose(leftside, rightside)
+      wht4
+    }
   }
+
+  val dslprogram = new MyDSLProgram
+  val result = dslprogram.myprog()
+  println(result)
+
+
 }
