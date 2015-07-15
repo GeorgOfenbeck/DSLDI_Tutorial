14b4e6dad1470c1ddaececced640aec2eb135493
GeorgOfenbeck <ofenbeck@inf.ethz.ch>
2015-07-15 20:04:23 +0200

    starting to implement the DSL with LMS

---
+++ src/main/spirals/dsls/SPL_DSL.scala
@@ -0,0 +1,8 @@
+package ch.ethz.spirals.dsls
+import scala.lms._
+import scala.lms.internal.InternalFunctionsExp
+
+trait SPL_DSL extends BaseExp with InternalFunctionsExp {
+
+}
+
---
+++ src/main/tutorial/IntroDSLStaging.scala
@@ -0,0 +1,10 @@
+package tutorial
+
+import ch.ethz.spirals.dsls._
+import ch.ethz.spirals.rewrites._
+
+object IntroDSLStaging extends App {
+
+  val wht4 = (F_2 tensor I(2) ) compose (I(2) tensor F_2) //what we would like to have
+
+}
